f5d49c2053176a745c3403fb4823757333ad032c
Simone Attanasio <161967392+mrz1mm@users.noreply.github.com>
2025-04-15 16:52:53 +0200

    first commit

---
+++ backEnd/src/test/java/com/springBoot/saveMe/SaveMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springBoot.saveMe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaveMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
